dd235e50e0482aaa2afbd56cc4f749008f28dfab
Lukas Zech <lukas.zech.software@gmail.com>
2018-12-07 15:04:48 +0100

    build

--- src/MUIDataTableFilter.js
+++ src/MUIDataTableFilter.js
@@ -180,7 +180,11 @@ class MUIDataTableFilter extends React.Component {
     const textLabels = options.textLabels.filter;
 
     if (column.customFilterRender) {
-      return column.customFilterRender(filterList[index][0], (value)=>this.props.onFilterUpdate(index,value,"dropdown"), classes.selectFormControl);
+      return column.customFilterRender(
+        filterList[index][0],
+        value => this.props.onFilterUpdate(index, value, "dropdown"),
+        classes.selectFormControl,
+      );
     }
 
     return (
